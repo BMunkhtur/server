ff40d3dcb7246b78adc0440ff8433f546fbaa3ed
BMunkhtur <bayarsaikhanmunkhtur@gmail.com>
2023-03-13 13:01:54 +0800

    up

--- routes/wishlist.js
+++ routes/wishlist.js
@@ -2,10 +2,7 @@ const { Router } = require("express");
 const cors = require("cors");
 const fs = require("fs");
 const { getWishlist, deleteWishlist } = require("../controllers/wishlist");
-
 const router = Router();
-
 router.get("/", getWishlist);
 router.delete("/:id", deleteWishlist);
-
 module.exports = router;
